41083c901124a55b8d2a64fb21f49569944d7680
Kolin Hurlock <hurlock.kolin@gmail.com>
2018-04-11 16:43:59 -0700

    update files

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,9 +1,9 @@
-var inquirer = require('inquirer');
-var mysql = require('mysql');
+var inquirer = require('inquirer')
+var mysql = require('mysql')
 
-var amountOwed;
-var currentDepartment;
-var updateSales;
+var amountOwed
+var currentDepartment
+var updateSales
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -11,13 +11,13 @@ var connection = mysql.createConnection({
 	user: 'root',
 	password: 'root',
 	database: 'Bamazon_db'
-});
+})
 
 //Establish Connection
 connection.connect(function(err){
-	if (err) throw err;
+	if (err) throw err
 	console.log('connected as id: ' + connection.threadId)
-});
+})
 
 //FUNCTIONS
 //=============================================================================
@@ -25,16 +25,16 @@ connection.connect(function(err){
 //Displays all items available in store and then calls the place order function
 function showProducts(){
 	connection.query('SELECT * FROM products', function(err, res){
-		if (err) throw err;
-		console.log('=================================================');
-		console.log('=================Items in Store==================');
-		console.log('=================================================');
+		if (err) throw err
+		console.log('=================================================')
+		console.log('=================Items in Store==================')
+		console.log('=================================================')
 
 		for(i=0;i<res.length;i++){
 			console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + '$' + res[i].Price + '(Quantity left: ' + res[i].StockQuantity + ')')
 		}
-		console.log('=================================================');
-		placeOrder();
+		console.log('=================================================')
+		placeOrder()
 		})
 }
 
@@ -63,31 +63,31 @@ function placeOrder(){
 	}]).then(function(answer){
 	connection.query('SELECT * FROM products WHERE id = ?', [answer.selectId], function(err, res){
 		if(answer.selectQuantity > res[0].StockQuantity){
-			console.log('Insufficient Quantity');
-			console.log('This order has been cancelled');
-			console.log('');
-			newOrder();
+			console.log('Insufficient Quantity')
+			console.log('This order has been cancelled')
+			console.log('')
+			newOrder()
 		}
 		else{
-			amountOwed = res[0].Price * answer.selectQuantity;
-			currentDepartment = res[0].DepartmentName;
-			console.log('Thanks for your order');
-			console.log('You owe $' + amountOwed);
-			console.log('');
+			amountOwed = res[0].Price * answer.selectQuantity
+			currentDepartment = res[0].DepartmentName
+			console.log('Thanks for your order')
+			console.log('You owe $' + amountOwed)
+			console.log('')
 			//update products table
 			connection.query('UPDATE products SET ? Where ?', [{
 				StockQuantity: res[0].StockQuantity - answer.selectQuantity
 			},{
 				id: answer.selectId
-			}], function(err, res){});
+			}], function(err, res){})
 			//update departments table
-			logSaleToDepartment();
-			newOrder();
+			logSaleToDepartment()
+			newOrder()
 		}
 	})
 
 }, function(err, res){})
-};
+}
 
 //Allows the user to place a new order or end the connection
 function newOrder(){
@@ -97,32 +97,32 @@ function newOrder(){
 		message: 'Would you like to place another order?'
 	}]).then(function(answer){
 		if(answer.choice){
-			placeOrder();
+			placeOrder()
 		}
 		else{
-			console.log('Thank you for shopping at Bamazon!');
-			connection.end();
+			console.log('Thank you for shopping at Bamazon!')
+			connection.end()
 		}
 	})
-};
+}
 
 
 //functions to push the sales to the executive table
 function logSaleToDepartment(){
 	connection.query('SELECT * FROM departments WHERE DepartmentName = ?', [currentDepartment], function(err, res){
-		updateSales = res[0].TotalSales + amountOwed;
-		updateDepartmentTable();
+		updateSales = res[0].TotalSales + amountOwed
+		updateDepartmentTable()
 	})
-};
+}
 
 function updateDepartmentTable(){
 		connection.query('UPDATE departments SET ? WHERE ?', [{
 		TotalSales: updateSales
 	},{
 		DepartmentName: currentDepartment
-	}], function(err, res){});
-};
+	}], function(err, res){})
+}
 //Call the original function (all other functions are called within this function)
 //======================================================================
-showProducts();
+showProducts()
 
